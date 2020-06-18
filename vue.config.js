911281673cc4ee6549aacf1e1366892a99daae37
梁柱 <15622207904@163.com>
2020-06-18 12:32:57 +0800

    lz
    增加图片压缩

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,21 @@
 // vue.config.js
 module.exports = {
+  chainWebpack: (config) => {
+    config.module
+      .rule("images")
+      .use("image-webpack-loader")
+      .loader("image-webpack-loader")
+      .options({
+        bypassOnDebug: true,
+      })
+      .end();
+  },
   devServer: {
     host: "192.168.0.7",
     proxy: {
-        '/': {
-            target: 'http://120.79.31.26/', //API服务器的地址
-        }
+      "/": {
+        target: "http://120.79.31.26/", //API服务器的地址
+      },
     },
   },
 };
